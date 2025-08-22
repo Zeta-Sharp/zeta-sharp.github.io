6e30efa91fb893f94110b13ee77f93856a3a0292
Ζ# <rikeidanshi@duck.com>
2025-08-22 17:09:55 +0900

    Create style.cs

---
+++ style.cs
@@ -0,0 +1,10 @@
+@media (prefers-color-scheme: dark)
+{
+    body {background-color: #fff;}
+    h1 {color: #000;}
+    p {color: #000;}
+    
+}
+body {background-color: #000;}
+h1 {color: #fff;}
+p {color: #fff;}

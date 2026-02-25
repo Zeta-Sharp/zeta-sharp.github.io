fa2df126673232bb53721c7200d7c5bcdc4a62df
Ζ# <rikeidanshi@duck.com>
2026-02-25 12:00:38 +0900

    Add PyProma project details

---
+++ projects/script_projects_detail.js
@@ -0,0 +1,94 @@
+// Language Changing
+const htmlTag = document.querySelector('html');
+let isJapanese = localStorage.getItem('selectedLang') === 'ja' || navigator.language.startsWith('ja');
+let texts
+const languageButton = document.querySelector('.language-button');
+const languageButtonIcon = document.querySelector('.language-button-icon');
+const currentURL = window.location.href;
+const pathname = new URL(currentURL).pathname;
+const match = pathname.match(/projects\/(.+)\.html$/);
+const slag = match[1];
+
+languageButton.addEventListener('click', () => {
+    isJapanese = !isJapanese;
+    updateLanguage();
+});
+languageButtonIcon.addEventListener('click', () => {
+    isJapanese = !isJapanese;
+    updateLanguage();
+});
+
+async function loadLanguageFile() {
+    try {
+        const response = await fetch(`./project_data.json`);
+        projects = await response.json();
+    }
+    catch (error) {
+        console.error('Error loading project data file:', error);
+    }
+    try {
+        if (!slag) console.error('Project slug not found in URL');
+        if (!projects[slag]) console.error(`Project data not found for slug: ${slag}`);
+        const response = await fetch(`./${slag}.json`);
+        texts = await response.json();
+        updateLanguage();
+    }
+    catch (error) {
+        console.error('Error loading language file:', error);
+    }
+}
+
+function updateLanguage() {
+    if (!texts) return;
+    const lang = isJapanese ? 'ja' : 'en';
+    localStorage.setItem('selectedLang', lang);
+    htmlTag.setAttribute('lang', lang);
+    languageButton.textContent = isJapanese ? '日→En Switch to English' : 'En→日 日本語に切り替え';
+    document.querySelectorAll('[data-i18n]').forEach(translatableElement => {
+        const key = translatableElement.getAttribute('data-i18n');
+        const type = translatableElement.getAttribute('data-i18n-type')
+        const keys = key.split('.');
+
+        let translation = texts;
+
+        const found = keys.every(k => {
+            if (translation && translation[k] !== undefined) {
+                translation = translation[k];
+                return true;
+            }
+            return false;
+        });
+
+        if (found && translation[lang] !== undefined) {
+            if (type === 'html') {
+                translatableElement.innerHTML = translation[lang];
+            } else {
+                translatableElement.textContent = translation[lang];
+            }
+        }
+    });
+}
+
+// Header Buttons
+
+const iconElement = document.querySelector('body > header img');
+const titleElement = document.querySelector('body > header h1');
+const profileButton = document.querySelector('.profile-button');
+const blogButton = document.querySelector('.blog-button');
+
+iconElement.addEventListener('click', () => {
+    location.href = 'https://zeta-sharp.github.io/';
+});
+titleElement.addEventListener('click', () => {
+    location.href = 'https://zeta-sharp.github.io/';
+});
+profileButton.addEventListener('click', () => {
+    location.href = 'https://zeta-sharp.github.io/';
+});
+blogButton.addEventListener('click', () => {
+    location.href = 'https://zeta-sharp.github.io/blog/';
+});
+
+
+loadLanguageFile();
+htmlTag.removeAttribute('translate')
